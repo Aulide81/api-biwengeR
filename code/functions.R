7496c7dd7d72b5eaa2f9aedf3c699c09d89365fe
Aulide81 <emiliojat@gmail.com>
2023-09-04 10:21:31 +0200

    Update functions.R

--- code/functions.R
+++ code/functions.R
@@ -79,8 +79,12 @@ get_movements<-function(token, list_ids, members, amount_init=50000000, step=500
   continue<-TRUE
   while (continue){
     
-    url<-gsub("__OFFSET__",OFFSET,url)
-    response = GET(url, add_headers(authorization = paste('Bearer', token), `x-league` = list_ids$id_league, `x-user` = list_ids$id_user))
+    #url<-gsub("__OFFSET__",OFFSET,url)
+    response = GET( gsub("__OFFSET__",OFFSET,url), 
+                   add_headers(authorization = paste('Bearer', token), 
+                               `x-league` = list_ids$id_league, 
+                               `x-user` = list_ids$id_user))
+    
     lista<-content(response)$data
     
     for (div in lista){
