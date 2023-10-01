fa5f2f6c29c3ce3ae9dbf062baf6a300c88ed3df
Aulide81 <emiliojat@gmail.com>
2023-10-02 00:23:53 +0200

    Update old_functions.R

--- code/old_functions.R
+++ code/old_functions.R
@@ -1,1 +1,165 @@
+library(httr)
 
+get_token <- function(email, password){
+  
+  response <- POST( url="https://biwenger.as.com/api/v2/auth/login" , 
+                    body = list("email"= email, "password"= password))
+  
+  token<-content(response)
+  
+  return(token$token)
+  
+}
+
+get_leagues<-function(token){
+  
+  response <- GET(url="https://biwenger.as.com/api/v2/account", 
+                  add_headers(authorization = paste('Bearer', token)))
+  
+  lista <- content(response)
+  ligas <- sapply(lista$data$leagues, function(l){
+    c("League Name"=l$name,"Id League"=l$id,"User Name"=l$user$name,"Id User"=l$user$id)} )
+  ligas<-t(ligas)|>data.frame()
+  return(ligas)
+}
+
+set_ids<-function(list_ligues, nleague){
+  lista<-list('id_league'=as.numeric(list_ligues[nleague,2]),
+              'id_user'=as.numeric(list_ligues[nleague,4]))
+}
+
+set_ids2<-function(list_ligues, id.league){
+  
+  id.league<-as.numeric(id.league)
+  id.user <- as.numeric(list_ligues$Id.User[list_ligues$Id.League==id.league])
+  
+  return(list('id_league'= id.league,'id_user'= id.user))
+}
+
+get_members<-function(token, list_ids){
+  
+  response<-GET(url="https://biwenger.as.com/api/v2/league?include=all,-lastAccess&fields=*,standings,tournaments,group,settings(description)",
+                add_headers(authorization = paste('Bearer', token), `x-league` = list_ids$id_league, `x-user` = list_ids$id_user))
+  
+  lista <- content(response)
+  
+  icon_league<-paste0("https://cdn.biwenger.com/",lista$data$icon)
+  
+  miembros<-sapply(lista$data$standings, function(p){
+    c("Id"=p$id,
+      "Name"=p$name,
+      "Points"=p$points,
+      "Size"=p$teamSize,
+      "Value"=p$teamValue,
+      "icon"=paste0("https://cdn.biwenger.com/",p$icon))
+  })|>t()|>data.frame()
+  
+  miembros<-structure(miembros,"create_date"=lista$data$created, "icon_league"=icon_league)
+  
+  return(miembros)
+  
+}
+
+get_movements<-function(token, list_ids, members, amount_init=50000000, step=500){
+  
+  # Inicializamos el balance
+  amount_init<-amount_init
+  create_date <- attr(members,"create_date")
+  balance<-lapply(members$Name, function(p){
+    m <- c(amount_init)
+    names(m) <- create_date
+    return(m)
+  })
+  names(balance)<-members$Name
+  
+  url<-"https://biwenger.as.com/api/v2/league/__league__/board?offset=__OFFSET__&limit=__step__"
+  url<-gsub("__league__",list_ids$id_league,url)
+  url<-gsub("__step__",step,url)
+  
+  OFFSET<-0
+  continue<-TRUE
+  while (continue){
+    
+    response = GET( gsub("__OFFSET__",OFFSET,url), 
+                   add_headers(authorization = paste('Bearer', token), 
+                               `x-league` = list_ids$id_league, 
+                               `x-user` = list_ids$id_user))
+    
+    lista<-content(response)$data
+    
+    for (div in lista){
+      
+      if ( div$type %in% c("leagueReset", "leagueWelcome") ) {
+        continue<-FALSE
+        break
+      }
+      
+      if (div$type %in% c("transfer","market", "roundFinished")){
+        
+        if (div$type %in% c("transfer","market")){
+        
+        position<-1
+        for(mov in div$content){
+          
+          #Existe comprador
+          if ('to' %in% names(mov)){
+            if (div$date %in% names(balance[[mov$to$name]])){
+              amount<-c(-mov$amount)
+              names(amount)<- (div$date + position)
+              balance[[mov$to$name]]<-c(balance[[mov$to$name]],amount)
+            }else{
+              amount<-c(-mov$amount)
+              names(amount)<- (div$date)
+              balance[[mov$to$name]]<-c(balance[[mov$to$name]],amount)
+            }
+          }
+          #Existe vendedor
+          if ('from' %in% names(mov)){
+            if (div$date %in% names(balance[[mov$from$name]])){
+              amount<-c(mov$amount)
+              names(amount) <- (div$date + position)
+              balance[[mov$from$name]]<-c(balance[[mov$from$name]],amount)
+            }else{
+              amount <- c(mov$amount)
+              names(amount) <- (div$date)
+              balance[[mov$from$name]]<-c(balance[[mov$from$name]],amount)
+            }
+          }
+        }
+        
+      }else{
+        
+        # Los bonus de jornada
+        mov <- div$content
+        jornada <- mov$round$name
+        
+        for (resultado in mov$results){
+          
+          if (jornada %in% names(balance[[resultado$user$name]])){
+            
+            #bonus<-c(resultado$bonus)
+            #balance[[resultado$user$name]][jornada] <- bonus
+            dummy=1
+            
+          }else{
+            
+            bonus<-c(resultado$bonus)
+            names(bonus) <- jornada
+            balance[[resultado$user$name]] <- c(balance[[resultado$user$name]], bonus)
+            
+          }
+        }
+      }
+    }
+  }
+    OFFSET<-OFFSET+step
+    
+    #if (length(lista) < step) {
+    #  continue <- FALSE
+    #}
+    
+    Sys.sleep(10)
+}
+  
+  return(balance)
+}
